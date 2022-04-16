ed0ce42514951710eb2a2dc97c6903d675912ba2
Fujikun <fujisawa3838@gmail.com>
2022-04-16 15:54:14 +0900

    ランダムなサイコロの目を出す右の画像も

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var images = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"]
-var randomNumber1 = Math.floor(Math.random() * images.length)
-
-document.querySelector(".img1").src = images[randomNumber1]
+var images = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
+var randomNumber1 = Math.floor(Math.random() * images.length);
+var randomNumber2 = Math.floor(Math.random() * images.length);
+document.querySelector(".img1").src = images[randomNumber1];
+document.querySelector(".img2").src = images[randomNumber2];
