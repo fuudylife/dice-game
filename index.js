2147bcb9e51ccca3fdbab291d425f17436b85ed9
Fujikun <fujisawa3838@gmail.com>
2022-04-16 15:33:07 +0900

    ランダムなサイコロの目を出す

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1
+var images = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"]
+var randomNumber1 = Math.floor(Math.random() * images.length)
+
+document.querySelector(".img1").src = images[randomNumber1]
