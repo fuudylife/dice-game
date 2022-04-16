b21041a65ad0592beca04ecd197c3d5c56ce1df4
Fujikun <fujisawa3838@gmail.com>
2022-04-16 16:42:49 +0900

    勝敗をつける

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ var randomNumber1 = Math.floor(Math.random() * images.length);
 var randomNumber2 = Math.floor(Math.random() * images.length);
 document.querySelector(".img1").src = images[randomNumber1];
 document.querySelector(".img2").src = images[randomNumber2];
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 player 1 Win!";
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "player 2 Win! 🚩";
+} else {
+    document.querySelector("h1").innerHTML = "Drow";
+}
