ee13a0b6de17776419aac6c87e9a083be6046912
Fujikun <fujisawa3838@gmail.com>
2022-04-16 17:20:38 +0900

    自作コードコメントアウト

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+
+// 自作のもの
+/*
+var images = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
+var randomNumber1 = Math.floor(Math.random() * images.length);
+var randomNumber2 = Math.floor(Math.random() * images.length);
+document.querySelector(".img1").src = images[randomNumber1];
+document.querySelector(".img2").src = images[randomNumber2];
+*/
+
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png";
@@ -23,3 +33,11 @@ if (randomNumber1 > randomNumber2) {
 } else {
     document.querySelector("h1").innerHTML = "Drow!";
 }
+
+/*
+var images = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
+var randomNumber1 = Math.floor(Math.random() * images.length);
+var randomNumber2 = Math.floor(Math.random() * images.length);
+document.querySelector(".img1").src = images[randomNumber1];
+document.querySelector(".img2").src = images[randomNumber2];
+*/
